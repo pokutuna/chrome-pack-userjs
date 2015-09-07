34e4cfcccc95c1c48c8485eba0ed88e855e70a90
pokutuna <popopopopokutuna@gmail.com>
2015-09-07 11:09:21 +0900

    use js library from ts

---
+++ src/lib/lib1.d.ts
@@ -0,0 +1,7 @@
+declare module Lib1 {
+    export class Lib1 {
+        constructor(message: string);
+        message: string;
+    }
+}
+export = Lib1;
--- src/sample4-use-lib-ts/sample4-use-lib-ts.ts
+++ src/sample4-use-lib-ts/sample4-use-lib-ts.ts
@@ -1,3 +1,6 @@
+/// <reference path="../lib/lib1.d.ts" />
+import lib1 = require('../lib/lib1');
 import lib2 = require('../lib/lib2');
 console.log('sample4-use-lib-ts');
+new lib1.Lib1Sample('from sample4-use-lib-ts');
 new lib2.Lib2Sample('from sample4-use-lib-ts');
